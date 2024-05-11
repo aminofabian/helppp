bf6ecd65e04493bbaaf14ca3f69fdd6dda325a29
Fabian Amino <aminofab@gmail.com>
2024-05-11 16:46:29 +0300

    added

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
     <span className="sr-only">Error</span>404
     </h2>
     <p className="text-2xl font-semibold md:text-3xl">Oops! You've Wandered Off the Map</p>
-    <p className="mt-4 mb-8 dark:text-gray-600">But fret not, intrepid explorer! While you're here, why not join us for a round of digital hide-and-seek? Or perhaps a game of virtual hot potato? We promise, it's way more fun than trying to find that missing webpage.</p>
+    <p className="mt-4 mb-8 dark:text-gray-600">But fret not, intrepid explorer! While you're here, why not join us for a round of digital hide-and-seek? Or perhaps a game of virtual hot potato? We promise, it's way more fun than trying to find that missing webpage...</p>
     <a rel="noopener noreferrer" href="/" className="px-8 py-3 font-semibold rounded dark:bg-violet-600 dark:text-gray-50">Click Here, Let's Get You Back Home</a>
     </div>
     </div>
