eca5c7f5ce38a86749f702e921738c08668bee0f
Fabian Amino <aminofab@gmail.com>
2025-01-27 12:18:31 +0300

    added

--- app/_components/CopyLink.tsx
+++ app/_components/CopyLink.tsx
@@ -5,7 +5,12 @@ import { useToast } from '@/components/ui/use-toast';
 import { Share, ShareIcon } from 'lucide-react'
 import React from 'react'
 
-export default function CopyLink({ id }: { id: string }) {
+interface CopyLinkProps {
+  id: string;
+  className?: string;
+}
+
+export default function CopyLink({ id, className }: CopyLinkProps) {
   const {toast} = useToast()
   async function copyToClipboard() {
     await navigator.clipboard.writeText(`${location.origin}/request/${id}`)
@@ -16,18 +21,16 @@ export default function CopyLink({ id }: { id: string }) {
     })
   }
   
-  
   return (
     <div className='flex'>
-    <Button 
-    onClick={copyToClipboard}
-    variant="secondary"
-    size="sm"
-    className="bg-green-100 hover:bg-green-200 text-green-700"
-    >
-    <ShareIcon className="w-4 h-4" />
-    </Button>
-    
+      <Button 
+        onClick={copyToClipboard}
+        variant="secondary"
+        size="sm"
+        className={`bg-green-100 hover:bg-green-200 text-green-700 ${className || ''}`}
+      >
+        <ShareIcon className="w-4 h-4" />
+      </Button>
     </div>
   )
 }
