9cffc4326846f91e321fd329dbc724db3dce7551
Fabian Amino <aminofab@gmail.com>
2025-02-25 14:48:18 +0300

    added

--- app/page.tsx
+++ app/page.tsx
@@ -12,20 +12,20 @@ const ShowItems = dynamic(() => import("./_components/ShowItems").then(mod => mo
 
 export default function Home({ searchParams }: { searchParams: { page: string } }) {
   return (
-    <div className="w-full md:container md:px-6 mx-auto max-w-7xl">
+    <div className="w-full md:container md:px-6 mx-auto max-w-7xl bg-white dark:bg-gray-900">
       <div className="grid grid-cols-1 gap-2 md:gap-4 lg:grid-cols-4 lg:gap-6">
-        <div className="h-fit rounded-none md:rounded-lg border-x-0 md:border border-green-50/20 floating hidden md:block order-last lg:sticky lg:top-20 z-10">
+        <div className="h-fit rounded-none md:rounded-lg border-x-0 md:border border-green-50/20 floating hidden md:block order-last lg:sticky lg:top-20 z-10 bg-white dark:bg-gray-900">
           <RightNavHome />
         </div>
         
-        <div className="h-fit rounded-none md:rounded-lg lg:col-span-2 order-2 space-y-2 md:space-y-4 relative z-20">
+        <div className="h-fit rounded-none md:rounded-lg lg:col-span-2 order-2 space-y-2 md:space-y-4 relative z-20 bg-white dark:bg-gray-900">
           <CreatePostCard />
           <Suspense fallback={<SuspenseCard />}>
             <ShowItems />
           </Suspense>
         </div>
         
-        <div className="h-fit rounded-none md:rounded-lg sticky top-0 md:top-20 shadow-lg border-x-0 md:border border-green-50/20 p-3 md:p-4 lg:p-5 order-first lg:order-none bg-white/95 dark:bg-gray-900/95 backdrop-blur-md z-10">
+        <div className="h-fit rounded-none md:rounded-lg sticky top-0 md:top-20 shadow-lg border-x-0 md:border border-green-50/20 p-3 md:p-4 lg:p-5 order-first lg:order-none bg-white dark:bg-gray-900 z-10">
           <HomeNavRightWrapper />
         </div>
       </div>
